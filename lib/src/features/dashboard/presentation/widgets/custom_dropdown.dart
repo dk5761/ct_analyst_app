8a62d074ec85049a77f612d2e769960f858132d0
dk5761 <darshankadam8@gmail.com>
2022-08-22 14:02:53 +0530

    adding the dropdown to table data

---
+++ lib/src/features/dashboard/presentation/widgets/custom_dropdown.dart
@@ -0,0 +1,53 @@
+// change it to riverpod class
+
+import 'package:ct_analyst_app/src/features/dashboard/data/dashboard_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../../constants/colors.dart';
+
+// ignore: must_be_immutable
+class CustomDropDown extends ConsumerStatefulWidget {
+  List<dynamic>? names;
+  String? value;
+
+  CustomDropDown({
+    Key? key,
+    required this.value,
+    required this.names,
+  }) : super(key: key);
+
+  @override
+  CustomDropDownState createState() => CustomDropDownState();
+}
+
+class CustomDropDownState extends ConsumerState<CustomDropDown> {
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: widget.value,
+      borderRadius: const BorderRadius.all(Radius.circular(12)),
+      elevation: 16,
+      style: const TextStyle(color: darkHeaderTextColor),
+      underline: Container(
+        height: 0,
+        color: darkTextEnableFocusColor,
+      ),
+      onChanged: (String? newValue) {
+        ref.read(dropItemProvider.notifier).state = newValue as String;
+        // ref.refresh(fetchDashboardData(newValue));
+        setState(() {
+          widget.value = newValue;
+        });
+      },
+      items: widget.names!.map<DropdownMenuItem<String>>((dynamic value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Padding(
+              padding: const EdgeInsets.all(8), child: Text(value as String)),
+        );
+      }).toList(),
+    );
+  }
+}
+
+final dropItemProvider = StateProvider<String>((ref) => "");
---
+++ lib/src/features/dashboard/presentation/widgets/name_dropdown_list.dart
@@ -0,0 +1,27 @@
+//drop downlist widget for analyst names.
+import 'package:ct_analyst_app/src/features/dashboard/data/dashboard_repository.dart';
+import 'package:ct_analyst_app/src/features/dashboard/presentation/widgets/custom_dropdown.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AnalystNameDropdown extends ConsumerWidget {
+  const AnalystNameDropdown({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // final names = ref.watch(fetchNamesData);
+    String? positionValue;
+
+    return Container();
+    // return names.when(
+    //     data: (data) {
+    //       // final namesList = ref.read(dashboardRepositoryProvider).namesList;
+
+    //       return CustomDropDown(value: positionValue, names: namesList);
+    //     },
+    //     error: (error, _) {
+    //       return Text(error.toString());
+    //     },
+    //     loading: () => const CircularProgressIndicator());
+  }
+}
